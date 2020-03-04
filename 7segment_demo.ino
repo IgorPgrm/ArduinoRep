56d65626663afb48fe6b019cad98351675f0f050
IgorShum <Shumeikoigor@yandex.ru>
2020-03-04 22:29:48 +0300

    Config PINs

--- 7segment_demo.ino
+++ 7segment_demo.ino
@@ -1,6 +1,21 @@
-
+int a = 8;
+int b = 9;
+int c = 4;
+int d = 3;
+int e = 2;
+int f = 7;
+int g = 6;
+int h = 5;
 
 void setup(){
+pinMode(a, OUTPUT);
+pinMode(b, OUTPUT);
+pinMode(c, OUTPUT);
+pinMode(d, OUTPUT);
+pinMode(e, OUTPUT);
+pinMode(f, OUTPUT);
+pinMode(g, OUTPUT);
+pinMode(h, OUTPUT);
 }
 
 void loop(){
