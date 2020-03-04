1055e0f44103acf6dbf518dc807299177658f8ad
IgorShum <Shumeikoigor@yandex.ru>
2020-03-04 22:38:32 +0300

    +Добавлена основная функция демо

--- 7segment_demo.ino
+++ 7segment_demo.ino
@@ -18,9 +18,6 @@ pinMode(g, OUTPUT);
 pinMode(h, OUTPUT);
 }
 
-void loop(){
-}
-
 void OnAllSeg(){
 digitalWrite(a,HIGH);
 digitalWrite(b,HIGH);
@@ -41,4 +38,89 @@ digitalWrite(e,LOW);
 digitalWrite(f,LOW);
 digitalWrite(g,LOW);
 digitalWrite(h,LOW);
+}
+
+void loop(){
+setup(); 	//Установка пинов
+int C=9;	//Отсчет от 9
+Serial.begin(9600);
+while(true){	//Вечный цикл
+  delay(250);	//Пауза 1\4 сек
+  C--;
+    OffAllSeg(); // Выкл все сегменты
+if (C<0){C=9;}else	//Если 0 то присваиваем 9
+Serial.print("if C==");
+Serial.print(C);
+switch(C){	//Загораются сегменты равные переменной "C"
+case 0:
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  break;
+case 1:
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  break;
+case 2:
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(g,HIGH);
+    break;
+case 3:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+case 4:
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+case 5:
+    digitalWrite(a,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+case 6:
+    digitalWrite(a,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+case 7:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+      break;
+case 8:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(e,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+case 9:
+    digitalWrite(a,HIGH);
+    digitalWrite(b,HIGH);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,HIGH);
+    digitalWrite(f,HIGH);
+    digitalWrite(g,HIGH);
+      break;
+}
+}
 }
